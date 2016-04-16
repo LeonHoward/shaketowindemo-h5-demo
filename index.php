2afce098f8a7be33b89bba0c5af16a760b084ecb
Leon <969196087@qq.com>
2016-04-16 11:02:03 +0800

    case of absolute positioning

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+<!--WeChat
+====================================================== -->
+<?php include_once 'weChat/weChatShareJS.php'; ?>
+</script>
+</body>
+</html>
